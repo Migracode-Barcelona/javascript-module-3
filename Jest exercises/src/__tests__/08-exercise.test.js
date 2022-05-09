abd901f2ec70509e30c17470b9d5a8e083d3b85f
Burçak <89997401+burcakkok@users.noreply.github.com>
2022-05-09 21:56:07 +0200

    jest exercises 7-8-9-10 done

--- Jest exercises/src/__tests__/08-exercise.test.js
+++ Jest exercises/src/__tests__/08-exercise.test.js
@@ -7,6 +7,7 @@ describe("08-exercises", () => {
   let addSpy = null;
 
   beforeAll(() => {
+    addSpy = jest.spyOn(calculator, 'add'); 
     // 1. Spy on the 'add' method of the calculator object
     //    and save the spy in the `addSpy` variable
   });
@@ -14,6 +15,7 @@ describe("08-exercises", () => {
   afterAll(() => addSpy.mockRestore());
 
   test("increment calls the calculator add method", () => {
+    increment(4, 5, calculator);
     /**
      * 2. Execute the `increment` by passing the arguments: (4, 5, calculator)
      */
@@ -22,8 +24,12 @@ describe("08-exercises", () => {
     expect.assertions(2);
 
     // 3. Make an assertion that the addSpy has been called 1 time
+    expect(addSpy.mock.calls.length).toEqual(1);
+
 
     // 4. Make an assertion that the addSpy has been called with the
     // numbers 4 and 5
+
+    expect(addSpy).toHaveBeenCalledWith(4, 5);
   });
 });
--- Jest exercises/src/__tests__/09-exercise.test.js
+++ Jest exercises/src/__tests__/09-exercise.test.js
@@ -7,14 +7,18 @@ describe("09-exercises", () => {
   let addSpy = null;
 
   beforeAll(() => {
+    addSpy = jest.spyOn(calculator, 'add');
     // 1. Spy on the 'add' method of the calculator object
     //    and save the spy in the `addSpy` variable
   });
 
   // 2. Clear the mock calls to the addSpy so that the tests work
-  beforeEach(() => {});
+  beforeEach(() => {
+    addSpy.mockClear();
+  });
 
   // 3. Restore the initial implementation of the addSpy using .mockRestore()
+
 
   test("increment returns 10 for 7 + 3", () => {
     let result = null;
@@ -24,15 +28,23 @@ describe("09-exercises", () => {
      *    and store the result in the `result` variable
      */
 
+    result = increment(7, 3, calculator);
+
     // You should make 3 assertions in this test, see bellow
     expect.assertions(3);
 
     // 5. Make an assertion that the `result` is 10
 
+    expect(result).toBe(10);
+
     // 6. Make an assertion that the addSpy has been called 1 time
+
+    expect(addSpy.mock.calls.length).toEqual(1);
 
     // 7. Make an assertion that the addSpy has been called with the
     //    numbers 7 and 3
+
+    expect(addSpy).toHaveBeenCalledWith(7,3)
   });
 
   test("increment returns 15 for 10 + 5", () => {
@@ -42,15 +54,19 @@ describe("09-exercises", () => {
      * 8. Execute the `increment` function by passing the arguments: (10, 5, calculator)
      *    and store the result in the `result` variable
      */
+    result = increment(10, 5, calculator);
 
     // You should make 3 assertions in this test, see bellow
     expect.assertions(3);
 
     // 9. Make an assertion that the `result` is 15
+    expect(result).toBe(15);
 
     // 10. Make an assertion that the addSpy has been called 1 time
+    expect(addSpy.mock.calls.length).toEqual(1);
 
     // 11. Make an assertion that the addSpy has been called with the
     //     numbers 10 and 5
+    expect(addSpy).toHaveBeenCalledWith(10, 5);
   });
 });
