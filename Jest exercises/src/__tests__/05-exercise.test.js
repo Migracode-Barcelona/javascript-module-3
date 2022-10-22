6700982bb458c87783ff773a5230444ed6cabf56
Amanda Maria <amanda.moov@gmail.com>
2022-10-22 17:29:15 +0200

    add ex 5

--- Jest exercises/src/__tests__/05-exercise.test.js
+++ Jest exercises/src/__tests__/05-exercise.test.js
@@ -25,6 +25,8 @@ describe("05-exercises", () => {
      */
 
     // Write the assertion
+    let result = addItemToList(prevList, newItem);
+    expect(result).toEqual([1, 2, 3, 4, 5])
   });
 
   test("addUser adds a new user to the list of users", () => {
@@ -46,6 +48,10 @@ describe("05-exercises", () => {
      */
 
     // Write the assertion
+    let result = addUser(users, expectedUser.name);
+    expect(result).toContainEqual({
+      name: "maria"
+    });
   });
 
   test("getWeekDays returns an array of week days", () => {
@@ -62,6 +68,10 @@ describe("05-exercises", () => {
      */
 
     // Write the assertion
+    let result = getWeekDays();
+    expect(result).toEqual(
+      expect.arrayContaining(expectedDays)
+    )
   });
 
   test("makeAdminUser returns an object with the role property", () => {
@@ -82,6 +92,10 @@ describe("05-exercises", () => {
      */
 
     // Write the assertion
+    let result = makeAdminUser(user);
+    expect(result).toEqual(
+      expect.objectContaining(expectedProperty)
+    )
   });
 
   test("getUserInfo returns an object without the address properties", () => {
@@ -107,5 +121,9 @@ describe("05-exercises", () => {
      */
 
     // Write the assertion
+    let result = getUserInfo(user);
+    expect(result).not.toEqual(
+      expect.objectContaining(userAddress)
+    )
   });
 });
